7f5f3d56c13842008d58928690922346d02f01af
Charly <dipta.biswas231998@gmail.com>
2019-09-03 14:57:23 +0530

    This is a simple Program to understanding Threads

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,25 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ThisThread thisThread=new ThisThread("Count");
-        Thread thread=new Thread(thisThread);
-        thread.start();
+        ThisThread thisThread=new ThisThread("Count");           //calling the Class where thread is implemanted;
+        Thread thread=new Thread(thisThread);                         // constructing the Thread with the class
+        thread.start();                                              // this will start the Thread
     }
 }
 
 class ThisThread implements Runnable{
-        String getThread;
+
+
+        String getThread;                                       //Initialize a String
+
+
+
     @Override
-    public void run() {
+    public void run() {                                         //  This is automaticly genarated method when we implementing Runnable;
         System.out.println(getThread+" is Starting ");
-        for(int c=0;c<10;c++){
+        for(int c=0;c<10;c++){                                  // Initialize for loot for completing the Threads;
             try {
-                Thread.sleep(1000);
+                Thread.sleep(1000);                        // Te=his helps to sleep Threads at the desired time as mentions
                 System.out.println(getThread+" is Counting Position "+c);
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -24,7 +29,7 @@ class ThisThread implements Runnable{
         }
     }
 
-    ThisThread(String string){
+    ThisThread(String string){                      // This will helps to set String value in Thread
         this.getThread=string;
     }
 }
